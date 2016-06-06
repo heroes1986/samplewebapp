881c609523363b17e3521e262a5cf2ab304bca57
kokeng <kokeng86@gmail.com>
2016-06-06 22:26:28 +0800

    add HomeService

---
+++ SampleWebApp/Services/HomeService.cs
@@ -0,0 +1,27 @@
+﻿namespace SampleWebApp.Services
+{
+    public interface IHomeService
+    {
+        string GetIndexName();
+        string GetAboutDescription();
+        string GetContractMessage();
+    }
+
+    public class HomeService : IHomeService
+    {
+        public string GetIndexName()
+        {
+            return "Index";
+        }
+
+        public string GetAboutDescription()
+        {
+            return "Your application description page.";
+        }
+
+        public string GetContractMessage()
+        {
+            return "Your contact page.";
+        }
+    }
+}
